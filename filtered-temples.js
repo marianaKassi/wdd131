30dc9568d14b83235575f0fa1c6800a91efe9a01
marianaKassi <Kassighislaine88@gmail.com>
2025-02-15 03:32:40 +0000

    Add files via upload

--- filtered-temples.js
+++ filtered-temples.js
@@ -1,106 +1,132 @@
-function createCard(location, details, imageUrl) {
-    const card = document.createElement('div');
-    card.className = 'card';
+const temples = [
+    {
+        name: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2015, August, 7",
+        area: "11,500 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg"     },
 
-    const img = document.createElement('img');
-    img.src = imageUrl;
-    img.alt = location;
-    img.loading = 'lazy';
-    img.onclick = () => showInfo(location, details);
+    {
+        name: "Manti",
+        location: "Manti, Utah",
+        dedicated: "1893",
+        area: "747 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/manti-utah-temple/manti-utah-temple-55303-thumb.jpg" 
+    },
+    {
+        name: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015",
+        area: "96,630 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/payson-utah-temple/payson-utah-temple-53786-thumb.jpg"
+    },
+       
+    {
+        name: "Accra Ghana",
+        location: "Accra, Grand Accra, Ghana",
+        dedicated: "January 11, 2004",
+        area: "76,450 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-15503-thumb.jpg"
+    },
+       
 
-    const title = document.createElement('h2');
-    title.textContent = location;
+    {
+     name: "Phoenix Arizona Temple", 
+     location: "Phoenix, Arizona", 
+     dedicated: "2014-12-13", 
+     area: 120000, 
+     image: "https://churchofjesuschristtemples.org/assets/img/temples/phoenix-arizona-temple/phoenix-arizona-temple-12711-main.jpg"
+       
+    },
 
-    const info = document.createElement('p');
-    info.textContent = details;
+    {
 
-    card.appendChild(img);
-    card.appendChild(title);
-    card.appendChild(info);
+name: "Seattle Washington Temple", 
+location: "Seattle, Washington", 
+dedicated: "1980-11-24", 
+area: 90000, 
+image: "https://churchofjesuschristtemples.org/assets/img/temples/seattle-washington-temple/seattle-washington-temple-14204-thumb.jpg"
 
-    return card;
-}
 
-function showInfo(location, details) {
-    alert(`Information about ${location}:\n${details}`);
-}
 
-const gallery = document.querySelector('.gallery');
-const places = [
-    { location: 'Aba, Nigeria', details: 'Dedicated: 2015, August, 7\nSize: 11,500 sq ft', imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg' },
-    { location: 'Manti, Utah', details: 'Dedicated: 1877\nSize: 748 sq ft', imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/manti-utah-temple/manti-utah-temple-55313.jpg' },
-    { location: 'Payson, Utah', details: 'Dedicated: 2015\nSize: 96,630 sq ft', imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/payson-utah-temple/payson-utah-temple-55109.jpg' },
-    { location: 'Yigo, Guam', details: 'Dedicated: 2019\nSize: 68,000 sq ft', imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/yigo-guam-temple/yigo-guam-temple-26505-thumb.jpg' },
-    {  location:'Mexico City Mexico',details: 'Dedicated: 1983, December 8\nSize: 116642 sq ft' , imageUrl:  'https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-20741-thumb.jpg' },
+    },
+    {
+        name: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020",
+        area: "68,000 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/yigo-guam-temple/yigo-guam-temple-26502-thumb.jpg"
+    },
+    
 
-    { location: 'Washington D.C Maryland, United States' ,details: 'Dedicated: 1974, November, 19\nSize: 156558 sq ft', imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/washington-d.c.-temple/washington-d.c.-temple-56743-thumb.jpg' },
+    {
 
-    { location: 'Lima, Perú',details:'Dedicated: 1986, January, 10\nSize: 9600 sq ft' ,imageUrl:'https://churchofjesuschristtemples.org/assets/img/temples/manaus-brazil-temple/manaus-brazil-temple-36798-thumb.jpg' },
+    name: "New York Temple",
+     location: "New York, New York", 
+     dedicated: "2004-12-15", 
+     area: 150000, 
+     image: "https://churchofjesuschristtemples.org/assets/img/temples/nauvoo-illinois-temple/nauvoo-illinois-temple-50576-main.jpg" },
+      
+
+
+    {
+        name: "London Englang Temple",
+        location: "London England",
+        dedicated: "7 September 1958",
+        area: "86,000 sq ft",
+        image:"https://churchofjesuschristtemples.org/assets/img/temples/london-england-temple/london-england-temple-55898.jpg"
+
+    },
+    
+
+
+    {
+
+    name: "Los Angeles Temple",
+    location: "Los Angeles, California", 
+    dedicated: "2000-04-13",
+    area: 191000,
+    image: "https://churchofjesuschristtemples.org/assets/img/temples/los-angeles-california-temple/los-angeles-california-temple-38945-main.jpg" },
+
+
+    {
+        name: "Rio De Janeiro Temple",
+        location: "Rio de Janeiro ,Brazil",
+        dedicated: "April 6, 2013",
+        area: "68,000 sq ft",
+        image:"https://churchofjesuschristtemples.org/assets/img/temples/rio-de-janeiro-brazil-temple/rio-de-janeiro-brazil-temple-8167.jpg"
+
+    },
+
+
+    {
+        name: "Paris France Temple",
+        location: "Paris,France",
+        dedicated: "May 21, 2017",
+        area: "9000 sq ft",
+        image: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-55206-thumb.jpg"
+    },
+   
 ];
 
-places.forEach(place => {
-    const card = createCard(place.location, place.details, place.imageUrl);
-    gallery.appendChild(card);
+const templeContainer = document.getElementById("temple-container");
 
-
-
-
-    const temples = [
-        {
-          templeName: "Aba Nigeria",
-          location: "Aba, Nigeria",
-          dedicated: "2005, August, 7",
-          area: 11500,
-          imageUrl:'https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg'
-        },
-        {
-          templeName: "Manti Utah",
-          location: "Manti, Utah, United States",
-          dedicated: "1888, May, 21",
-          area: 74792,
-          imageUrl: 'https://churchofjesuschristtemples.org/assets/img/temples/manti-utah-temple/manti-utah-temple-55313.jpg'
-        },
-        {
-          templeName: "Payson Utah",
-          location: "Payson, Utah, United States",
-          dedicated: "2015, June, 7",
-          area: 96630,
-          imageUrl:
-          'https://churchofjesuschristtemples.org/assets/img/temples/payson-utah-temple/payson-utah-temple-55109.jpg'
-        },
-          {
-          templeName: "Yigo Guam",
-          location: "Yigo, Guam",
-          dedicated: "2020, May, 2",
-          area: 6861,
-          imageUrl:
-          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
-        },
-        {
-          templeName: "Washington D.C.",
-          location: "Kensington, Maryland, United States",
-          dedicated: "1974, November, 19",
-          area: 156558,
-          imageUrl:
-          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
-        },
-        {
-          templeName: "Lima Perú",
-          location: "Lima, Perú",
-          dedicated: "1986, January, 10",
-          area: 9600,
-          imageUrl:
-          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
-        },
-        {
-          templeName: "Mexico City Mexico",
-          location: "Mexico City, Mexico",
-          dedicated: "1983, December, 2",
-          area: 116642,
-          imageUrl:
-          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
-        },
-      ];
-
-
+temples.forEach(temple => {
+    const templeCard = document.createElement("div");
+    templeCard.classList.add("temple-card");
+    templeCard.innerHTML = `
+        <img src="${temple.image}" alt="${temple.name}">
+        <h2>${temple.name}</h2>
+        <p>Location: ${temple.location}</p>
+        <p>Dedicated: ${temple.dedicated}</p>
+        <p>Size: ${temple.area}</p>
+    `;
+    templeContainer.appendChild(templeCard);
 });
+
+// Footer content
+const yearSpan = document.getElementById("year");
+const lastModifiedSpan = document.getElementById("last-modified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedSpan.textContent = document.lastModified;
